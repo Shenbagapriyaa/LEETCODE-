bde8873ac9722083bc03157301fbcd69c0d4f6e7
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-04 13:17:28 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,42 @@
+// Last updated: 6/4/2026, 1:17:24 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int c_5 = 0,c_10 = 0, c_20 = 0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i] == 5){
+                c_5++;
+            }
+            
+            else if(bills[i] == 10){
+                if(c_5>=1){
+                c_5--;
+                c_10++;
+            }
+                 else{
+                return false;
+                 }
+        }
+        
+            else if(bills[i] ==20){
+                if(c_10>=1 && c_5 >=1){
+                    c_10-=1;
+                    c_5-=1;
+                    c_20++;
+                }
+                else if(c_5>=3 ){
+                    c_5-=3;
+                    c_20++;
+                }
+            
+                
+                else{
+                    return false;   
+                }
+            }
+        }
+            
+        
+        return true;
+        
+    }
+}

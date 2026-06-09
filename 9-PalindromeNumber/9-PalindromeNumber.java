91cdc5de85f64ca93766eb650b3bd358d9bc6f2d
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-09 15:53:48 +0530

    File updated at 6/9/2026, 3:53:47 PM

--- 9-PalindromeNumber/9-PalindromeNumber.java
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -1,11 +1,18 @@
-// Last updated: 6/4/2026, 1:19:03 PM
-class Solution {
-    public boolean isPalindrome(int x) {
-        if(x<0)
-        return false;
-        String str=Integer.toString(x);
-        String rev=new StringBuilder(str).reverse().toString();
-        return str.equals(rev);
-        
-    }
-}
+// Last updated: 6/9/2026, 3:53:47 PM
+1class Solution {
+2    public boolean isPalindrome(int x) { 
+3        int org=x;
+4        int rev=0;
+5        while(x>0){
+6            int rem=x%10;
+7            rev=rev*10+rem;
+8            x/=10;
+9        }
+10        if(rev==org){
+11            return true;
+12        }
+13        else{
+14            return false;
+15        }
+16    }
+17}

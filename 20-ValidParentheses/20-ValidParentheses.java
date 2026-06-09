a1585076cae26bc644f528e5f6788c711603b0e4
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-09 14:05:39 +0530

    File updated at 6/9/2026, 2:05:38 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,23 +1,23 @@
-// Last updated: 6/4/2026, 1:18:53 PM
-class Solution {
-    public boolean isValid(String s) {
-        char[] stack = new char[s.length()];
-        int top = -1;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i); 
-            if (c == '(') {
-                stack[++top] = ')';
-            } else if (c == '{') {
-                stack[++top] = '}';
-            } else if (c == '[') {
-                stack[++top] = ']';
-            } else {
-                if (top == -1 || stack[top--] != c) {
-                    return false;
-                }
-            }
-        }
-        return top == -1;
-    }
-}
-    
+// Last updated: 6/9/2026, 2:05:38 PM
+1class Solution {
+2    public boolean isValid(String s) {
+3        Stack<Character> st = new Stack<>();
+4        for (int i = 0; i < s.length(); i++) {
+5            char ch = s.charAt(i);
+6            if (ch == '(' || ch == '{' || ch == '[') {
+7                st.push(ch);
+8            } else {
+9                if (st.isEmpty()) {
+10                    return false;
+11                }
+12                char top = st.pop();
+13                if ((ch == ')' && top != '(') ||
+14                    (ch == '}' && top != '{') ||
+15                    (ch == ']' && top != '[')) {
+16                    return false;
+17                }
+18            }
+19        }
+20        return st.isEmpty();
+21    }
+22}

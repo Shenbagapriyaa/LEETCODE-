8a648a3f8996cf1982548a8d126c1978513ca2a6
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-16 14:16:47 +0530

    File updated at 6/16/2026, 2:16:47 PM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
@@ -1,28 +1,19 @@
-// Last updated: 6/16/2026, 2:15:08 PM
+// Last updated: 6/16/2026, 2:16:47 PM
 1class Solution {
-2
-3    HashSet<String> set = new HashSet<>();
-4
-5    public boolean wordBreak(String s, List<String> wordDict) {
-6
-7        if(s.length() == 0)
-8            return true;
-9
-10        if(set.contains(s))
-11            return false;
-12
-13        for(int i = 0; i < wordDict.size(); i++) {
-14
-15            String word = wordDict.get(i);
-16
-17            if(s.startsWith(word)) {
-18
-19                if(wordBreak(s.substring(word.length()), wordDict))
-20                    return true;
-21            }
-22        }
-23
-24        set.add(s);
-25        return false;
-26    }
-27}
+2    HashSet<String> set = new HashSet<>();
+3    public boolean wordBreak(String s, List<String> wordDict) {
+4        if(s.length() == 0)
+5            return true;
+6        if(set.contains(s))
+7            return false;
+8        for(int i = 0; i < wordDict.size(); i++) {
+9            String word = wordDict.get(i);
+10            if(s.startsWith(word)) {
+11                if(wordBreak(s.substring(word.length()), wordDict))
+12                    return true;
+13            }
+14        }
+15        set.add(s);
+16        return false;
+17    }
+18}

92f6e4aec79900fa2032c682494442bffb162887
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-17 14:35:08 +0530

    File updated at 6/17/2026, 2:35:07 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
@@ -1,8 +1,15 @@
-// Last updated: 6/17/2026, 11:52:34 AM
+// Last updated: 6/17/2026, 2:35:07 PM
 1class Solution {
-2    public int findKthLargest(int[] nums, int k) {
-3    Arrays.sort(nums);
-4    
-5    return nums[nums.length-k];
-6    }
-7}
+2    public int firstUniqChar(String s) {
+3        int[] charCount = new int[26]; 
+4        for (char c : s.toCharArray()) {
+5            charCount[c - 'a']++;
+6        }
+7        for (int i = 0; i < s.length(); i++) {
+8            if (charCount[s.charAt(i) - 'a'] == 1) {
+9                return i; 
+10            }
+11        }
+12        return -1;
+13    }
+14} 

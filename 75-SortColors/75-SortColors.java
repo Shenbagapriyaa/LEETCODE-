86e73b647e6b18c0ff4a154109799191450202ed
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-23 15:59:34 +0530

    File updated at 6/23/2026, 3:59:32 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,6 +1,17 @@
-// Last updated: 6/23/2026, 3:03:32 PM
+// Last updated: 6/23/2026, 3:59:32 PM
 1class Solution {
 2    public void sortColors(int[] nums) {
-3        Arrays.sort(nums);
-4    }
-5}
+3       for(int i=0;i<nums.length-1;i++){
+4        for(int j=0;j<nums.length-i-1;j++){
+5            if(nums[j]>nums[j+1])    {
+6                int temp=nums[j];
+7                nums[j]=nums[j+1];
+8                nums[j+1]=temp;
+9
+10
+11            }   
+12        }
+13             
+14       }
+15    }
+16}

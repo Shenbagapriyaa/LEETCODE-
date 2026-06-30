381ccfa6dce4e3a7d643810a356b0805a96ae588
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-30 14:15:17 +0530

    File updated at 6/30/2026, 2:15:16 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,11 +1,23 @@
-// Last updated: 6/30/2026, 1:54:49 PM
+// Last updated: 6/30/2026, 2:15:16 PM
 1class Solution {
-2    public int titleToNumber(String s) {
-3         int ans = 0;
-4        for (int i = 0; i < s.length(); i++) {
-5            ans=ans*26+(s.charAt(i)-'A'+1);
-6        }
-7        return ans;
-8    }
-9   
-10}
+2    public boolean checkXMatrix(int[][] grid) {
+3        int n=grid.length;
+4        for(int i=0;i<n;i++){
+5            for(int j=0;j<n;j++){
+6                if(i==j || i+j==n-1){
+7                   if( grid[i][j]==0){
+8                    return false;
+9                   }
+10                }
+11                   else{
+12                    if( grid[i][j]!=0){
+13                        return false;
+14                    }
+15                   
+16
+17                }
+18            }
+19        }
+20        return true;
+21    }
+22}

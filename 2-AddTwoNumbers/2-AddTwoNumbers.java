75319dcd1527e0b64f4dc890889537f550be85d4
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-30 13:54:50 +0530

    File updated at 6/30/2026, 1:54:49 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,9 +1,9 @@
-// Last updated: 6/30/2026, 1:53:41 PM
+// Last updated: 6/30/2026, 1:54:49 PM
 1class Solution {
 2    public int titleToNumber(String s) {
 3         int ans = 0;
 4        for (int i = 0; i < s.length(); i++) {
-5            ans = ans * 26 + (s.charAt(i) - 'A' + 1);
+5            ans=ans*26+(s.charAt(i)-'A'+1);
 6        }
 7        return ans;
 8    }

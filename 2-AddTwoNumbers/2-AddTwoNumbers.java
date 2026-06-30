267f0f1d2aaa4f471cade80640c4fea010ce8924
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-06-30 13:53:42 +0530

    File updated at 6/30/2026, 1:53:41 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,13 +1,11 @@
-// Last updated: 6/30/2026, 12:34:29 PM
+// Last updated: 6/30/2026, 1:53:41 PM
 1class Solution {
-2    public int findMin(int[] nums) {
-3        Arrays.sort(nums);
-4        int min=nums[0];
-5        for(int i=0;i<nums.length;i++){
-6            if(nums[i]<min)
-7            min=nums[i];
-8        }
-9        return min;
-10        
-11    }
-12}
+2    public int titleToNumber(String s) {
+3         int ans = 0;
+4        for (int i = 0; i < s.length(); i++) {
+5            ans = ans * 26 + (s.charAt(i) - 'A' + 1);
+6        }
+7        return ans;
+8    }
+9   
+10}

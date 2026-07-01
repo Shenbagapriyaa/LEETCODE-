904132691dd4d8b826914b4179a55e231b68b05a
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-07-01 11:20:43 +0530

    File updated at 7/1/2026, 11:20:41 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -1,6 +1,5 @@
-# Last updated: 7/1/2026, 11:18:20 AM
-1class Solution:
-2    def reverseWords(self, s: str) -> str:
-3        return " ".join(s.split()[::-1])
+# Last updated: 7/1/2026, 11:20:41 AM
+1class Solution(object):
+2    def reverseWords(self, s):
+3        return " ".join(s.split() [::-1])
 4        
-5        

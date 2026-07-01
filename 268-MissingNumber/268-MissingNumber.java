880a44a008c7d1b42bb80c45bc21bed3dba04717
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-07-01 13:50:02 +0530

    File updated at 7/1/2026, 1:50:00 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,6 +1,22 @@
-// Last updated: 7/1/2026, 12:01:50 PM
+// Last updated: 7/1/2026, 1:50:00 PM
 1class Solution {
-2    public boolean canWinNim(int n) {
-3       return n%4!=0;
-4    }
-5}
+2    public int climbStairs(int n) {
+3        if(n<=0){
+4            return 0;
+5        }
+6        if(n==1){
+7            return 1;
+8        }
+9        if(n==2){
+10            return 2;
+11        }
+12       int a=1;
+13       int b=2;
+14       for(int i=3;i<=n;i++){
+15        int temp =a+b;
+16        a=b;
+17        b=temp;
+18       } 
+19       return b;
+20    }
+21}

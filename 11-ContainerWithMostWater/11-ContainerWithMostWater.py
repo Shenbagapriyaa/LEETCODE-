ab5dcda532b776588e48f0bf518cf71c599393fd
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-07-02 16:20:39 +0530

    File updated at 7/2/2026, 4:20:36 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,6 +1,6 @@
-# Last updated: 7/2/2026, 4:18:23 PM
+# Last updated: 7/2/2026, 4:20:36 PM
 1class Solution:
 2    def lengthOfLastWord(self, s: str) -> int:
-3        s=s.strip().split()
+3        s=s.split()
 4        return len(s[-1])
 5        

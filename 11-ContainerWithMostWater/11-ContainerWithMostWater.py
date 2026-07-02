54460b466505b67967473ffa0db873ea11f53d87
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-07-02 16:16:37 +0530

    File updated at 7/2/2026, 4:16:35 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,5 +1,6 @@
-# Last updated: 7/2/2026, 10:04:59 AM
-1class Solution(object):
-2    def repeatedSubstringPattern(self, s):
-3        return s in(s+s)[1:-1]
-4        
+# Last updated: 7/2/2026, 4:16:35 PM
+1class Solution:
+2    def lengthOfLastWord(self, s: str) -> int:
+3        s=s.strip().split()
+4        return len(s[-1])
+5        

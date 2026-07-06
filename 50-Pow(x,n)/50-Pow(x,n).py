659f997c42be231c495903d062edfe05dda155e5
Shenbagapriyaa <priyanarayanasamy2005@gmail.com>
2026-07-06 11:21:10 +0530

    File updated at 7/6/2026, 11:21:09 AM

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,4 +1,5 @@
-# Last updated: 7/6/2026, 11:20:33 AM
-1class Solution:
-2    def myPow(self, x: float, n: int) -> float:
+# Last updated: 7/6/2026, 11:21:09 AM
+1class Solution(object):
+2    def myPow(self, x, n):
 3        return pow(x,n)
+4        
